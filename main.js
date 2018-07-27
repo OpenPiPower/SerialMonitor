f4fc9aa26b20d2b412771abd21f5de3dc308c6e7
Damien Pattrick <damienpattrick@fedoralinux.connect>
2018-07-27 20:20:55 +0100

    Revised Upload (NOT WORKING)

--- main.js
+++ main.js
@@ -44,3 +44,6 @@ const {app, BrowserWindow} = require('electron')
       createWindow()
     }
   })
+  
+  // In this file you can include the rest of your app's specific main process
+  // code. You can also put them in separate files and require them here.
--- renderer.js
+++ renderer.js
@@ -1,6 +1,2 @@
-require("jquery");
-require("bootstrap");
-const remote = require("electron").remote;
-const app = remote.app;
 const serialport = require("serialport")
 var parser = new serialport.parsers.Readline("\r\n");
